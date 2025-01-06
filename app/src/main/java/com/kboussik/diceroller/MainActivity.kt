ee234f07e79a34a2b9de676bbb7204368c4f710e
Gearz <vlagamer1@gmail.com>
2025-01-06 11:09:02 +0100

    Dice Roll valid

---
+++ app/src/main/java/com/kboussik/diceroller/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.kboussik.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+    class Dice(private val numSides: Int) {
+
+        /**
+         * Do a random dice roll and return the result.
+         */
+        fun roll(): Int {
+            return (1..numSides).random()
+        }
+    }
