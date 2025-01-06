f9e374c5ed8e5a28c047a84f5df94958cee9261c
Gearz <vlagamer1@gmail.com>
2025-01-06 11:45:33 +0100

    Casino Done

--- app/src/main/java/com/kboussik/diceroller/MainActivity.kt
+++ app/src/main/java/com/kboussik/diceroller/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,13 +16,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val inputTextNumber : TextView = findViewById(R.id.editTextNumber)
         val rollButton: Button = findViewById(R.id.button)
+
+        //enable button when input is not empty
+        inputTextNumber.addTextChangedListener {
+            rollButton.isEnabled = it.toString().isNotEmpty()
+        }
+
         rollButton.setOnClickListener {
-            rollDice()
+            if (rollDice() == inputTextNumber.text.toString().toInt()) {
+                Toast.makeText(this, "You win!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "You lose!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
-    private fun rollDice() {
+    private fun rollDice():Int {
         // Create new Dice object with 6 sides and roll it
         val dice1 = Dice(6)
         val dice2 = Dice(6)
@@ -35,9 +47,7 @@ class MainActivity : AppCompatActivity() {
         val resultTextView2: TextView = findViewById(R.id.textView2)
         resultTextView2.text = diceRoll2.toString()
 
-        if (diceRoll1 == diceRoll2) {
-            Toast.makeText(this, "Double ! Congratulation !", Toast.LENGTH_SHORT).show()
-        }
+        return diceRoll1+diceRoll2
     }
 }
     class Dice(private val numSides: Int) {
